4fa61147650a47f05afccee29df271f8a3b714a4
mac_pilik <diablo2435@gmail.com>
2023-04-04 12:12:35 +0300

    Добавление blueprint

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@
 from flask import Flask
 from config import Configuration
 
+from posts.blueprint import posts #импортируем экземпрляр класса блюпринт
 
 app = Flask(__name__)
 app.config.from_object(Configuration) # передача в словарь конфиг настройки приложения через метод from_object (передаем класс Configuration)
+
+app.register_blueprint(posts, url_prefix='/blog')
--- app/view.py
+++ app/view.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
-@app.route('/') # декоратор, обращаемся к экземляру класса Фласк, методу роут - '/' - корень сайта, домен второго уровня
+@app.route('/') # декоратор, обращаемся к экземляру класса Фласк, методу роут - '/' - корень сайта, главная страница
 def index():
-    return render_template('index.html')
+    name = 'Ivan'
+    return render_template('index.html', n=name)
