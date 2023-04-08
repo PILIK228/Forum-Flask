09bc15b3f3ee373627036009f8a954e097cb8502
mac_pilik <diablo2435@gmail.com>
2023-04-08 10:58:25 +0300

    Added Search and Create Posts with html templates, but have some problems

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -2,17 +2,51 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post, Tag
+from .forms import PostForm
+
+from flask import request
+from app import db
+
+from flask import redirect
+from flask import url_for
+
 #конструктор класса блюпринт
 posts = Blueprint('posts',__name__, template_folder='templates')
 
 
+#http://localhost/blog/create
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.commit()
+        except:
+            print('Something wrong')
+
+        return redirect( url_for('posts.index') )
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    # получаем списоk постов
-    posts = Post.query.all()
+
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
 
 
+#http://localhost/blog/first-post
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
@@ -20,6 +54,7 @@ def post_detail(slug):
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 
+#http://localhost/blog/tag/python
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug==slug).first()
